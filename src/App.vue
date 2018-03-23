6b23d9514b66dee8376de7b657cdfa414e4e6267
hengmo <cjsoung@naver.com>
2018-03-23 15:07:56 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <header>
+      <span>ToDo list web
+        <img src="./assets/logo.png" alt="Vue.js PWA" height="45px" width="45px">
+        <a class="login" href="https://vuejs.org" target="_blank" rel="noopener">login</a>
+      </span>
+    </header>
+    
+    <main>
+      
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: rgb(0, 204, 255);
+  color: #ffffff;
+}
+
+header span {
+  display: inline;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 500;
+  box-sizing: border-box;
+  
+}
+</style>
